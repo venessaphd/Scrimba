f161f266b8a158ecde2e196878e2ca018f4ab5f6
venessaphd <75551146+venessaphd@users.noreply.github.com>
2023-01-20 15:56:20 -0500

    Refactored to simplify functions

--- sports counter/site/index.js
+++ sports counter/site/index.js
@@ -1,60 +1,40 @@
 // grab the elements
-let scoreHomeEl = document.getElementById("score-home-el")
-let scoreGuestEl = document.getElementById("score-guest-el")
-let celebrateHomeEl = document.getElementById("celebrate-home-el")
-let celebrateGuestEl = document.getElementById("celebrate-guest-el")
+let scoreHomeEl = document.getElementById("score-home-el");
+let scoreGuestEl = document.getElementById("score-guest-el");
+let celebrateHomeEl = document.getElementById("celebrate-home-el");
+let celebrateGuestEl = document.getElementById("celebrate-guest-el");
 
-let homeSum = 0
-let guestSum = 0
+let homeSum = 0;
+let guestSum = 0;
 
-function checkCelebrate() {
-        celebrateHomeEl.textContent = ""
-        celebrateGuestEl.textContent = ""
-    
-    if (homeSum > guestSum) {
-        celebrateHomeEl.textContent = "🎉 ✨ 🔥 🏅"
-        celebrateGuestEl.textContent = ":-("
-    } else {
-        celebrateHomeEl.textContent = "😞"
-        celebrateGuestEl.textContent = "⚽⚽⚽⚽🔥⚽⚽⚽⚽"
-    }
-}
-
-function add1home(){
-    homeSum += 1
-    scoreHomeEl.textContent = homeSum
-    checkCelebrate()
-}
-function add2home(){
-    homeSum += 2
-    scoreHomeEl.textContent = homeSum
-    checkCelebrate()
-}
-function add3home(){
-    homeSum += 3
-    scoreHomeEl.textContent = homeSum
-    checkCelebrate()
+function addPoints(team, points) {
+  //   let teamToScore = team + "Sum";
+  if (team === "home") {
+    homeSum += points;
+  } else {
+    guestSum += points;
+  }
+  scoreHomeEl.textContent = homeSum;
+  checkCelebration();
+  console.log("does it say homeSum?" + homeSum);
 }
 
-function add1guest(){
-    guestSum += 1
-    scoreGuestEl.textContent = guestSum
-    checkCelebrate()
-}
-function add2guest(){
-    guestSum += 2
-    scoreGuestEl.textContent = guestSum
-    checkCelebrate()
-}
-function add3guest(){
-    guestSum += 3
-    scoreGuestEl.textContent = guestSum
-    checkCelebrate()
+function checkCelebration() {
+  celebrateHomeEl.textContent = "";
+  celebrateGuestEl.textContent = "";
+
+  if (homeSum > guestSum) {
+    celebrateHomeEl.textContent = "🎉 ✨ 🔥 🏅";
+    celebrateGuestEl.textContent = ":-(";
+  } else {
+    celebrateHomeEl.textContent = "😞";
+    celebrateGuestEl.textContent = "⚽⚽⚽⚽🔥⚽⚽⚽⚽";
+  }
 }
 
-function newGame(){
-    homeSum = 0
-    guestSum = 0
-    scoreHomeEl.textContent = homeSum
-    scoreGuestEl.textContent = guestSum    
+function newGame() {
+  homeSum = 0;
+  guestSum = 0;
+  scoreHomeEl.textContent = homeSum;
+  scoreGuestEl.textContent = guestSum;
 }
